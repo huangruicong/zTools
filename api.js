771e4526a31ac7a3529e1a8dfc4515c99ec848af
ruiuii <huangruicong@foxmail.com>
2020-07-08 16:49:11 +0800

    Add files via upload
    update package.json

--- api.js
+++ api.js
@@ -1,8 +1,3 @@
 /**
  * Created by ruicong.huang on 2020/7/8.
  */
-export default {
-    test: function () {
-        console.log('this is a test output of zTools function')
-    }
-}
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 /**
  * Created by ruicong.huang on 2020/7/8.
  */
+export default {
+    test: function () {
+        console.log('this is a test output of zTools function')
+    }
+}
